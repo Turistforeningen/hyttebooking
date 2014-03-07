ee0bb54b4fc42a85b7f3a22ff57c1ce22683d22f
jhnoor <jamawadi@gmail.com>
2014-03-07 14:11:15 +0100

    Adding security validation file

---
+++ app/Security/Validation.java
@@ -0,0 +1,36 @@
+package Security;
+
+/**
+ * Defines input validation methods according to requirements
+ */
+public class Validation {
+
+	public static boolean isValidName(String name) {
+		
+		return false; //TODO
+	}
+	
+	public static boolean isValidEmail(String email) {
+		return false; //TODO
+	}
+	
+	public static boolean isValidPassword() {
+		return false; //TODO
+	}
+	
+	public static boolean isValidDob() {
+		return false; //TODO
+	}
+	
+	public static boolean isValidAddress() {
+		return false; //TODO
+	}
+	
+	public static boolean isValidCity() {
+		return false; //TODO
+	}
+	
+	public static boolean isValidZipCode() {
+		return false; //TODO
+	}
+}
