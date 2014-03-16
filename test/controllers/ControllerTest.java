320008b13833b136fe1fbc5ddaacc9dd6cf94d6f
jhnoor <jamawadi@gmail.com>
2014-03-16 17:33:52 +0100

    SmallCabin complete and tested, test runs successfully. LargeCabin runs into bugs with beds reserved, shows -1 to expected result in availability array. TODO: investigate

--- test/controllers/ControllerTest.java
+++ test/controllers/ControllerTest.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -69,7 +70,8 @@ public class ControllerTest extends WithApplication {
 		boolean[] bookedDays = new boolean[Math.abs(Days.daysBetween(startDate, endDate).getDays())+1];
 		JSONSerializer serializer = new JSONSerializer();
 		String actualResult;
-		String expectedResult = "bookedDays [true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]";
+		String expectedResult = "bookedDays [true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true]";
+		Boolean[] expectedResultBool = {true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true};
 		
 		Cabin cabin = Cabin.find.byId(cabinId);
 		if(cabin instanceof SmallCabin) {
@@ -86,18 +88,84 @@ public class ControllerTest extends WithApplication {
 					}
 				}
 				actualResult = ("bookedDays "+ serializer.serialize(bookedDays));
-				System.out.println(actualResult);
 			} else { //Either something is wrong or the entire given daterange shows available for given cabin
 				actualResult = ("bookedDays " + serializer.serialize(bookedDays));
-				System.out.println(actualResult);	
 			}
 		} else {
 			actualResult = (" status " + "KO ");
 			actualResult += ("message " + "date invalid");
 			System.out.println("Bad request: "+actualResult);
 		}
-		assertTrue(actualResult.equals(expectedResult));
+		if(cabin instanceof SmallCabin) {
+			System.out.println("CompareStrings: "+actualResult.compareTo(expectedResult));
+			assertTrue(0 == actualResult.compareTo(expectedResult));
+		}
+		else if(cabin instanceof LargeCabin)
+			assertTrue(false/**TODO**/);
 		
 		/** END TEST **/
+	}
+	
+	/**	BUGS: Expected results not similar to actual results, for some reason beds booked is -1 to expected result**/
+	@Test
+	public void TestGetAvailabilityForTimePeriodLargeCabin() {
+		LargeCabin lCabin = new LargeCabin("AvailabilityTest", 8);
+		lCabin.save();
+		User user = new User("q@t","w", "t");
+		user.save();
+		
+		//parameters that should be gotten from JSON object
+		DateTime startDate = new DateTime("2015-03-01");
+		DateTime endDate = new DateTime("2015-03-28");
+		long cabinId = lCabin.id;
+		
+		int[] largeCBookedDays = new int[Math.abs(Days.daysBetween(startDate, endDate).getDays())+1];
+		JSONSerializer serializer = new JSONSerializer();
+		String actualResult;
+
+		/** add different bookings and test **/
+		Date s1 = new DateTime("2015-03-01").toDate();
+		Date e1 = new DateTime("2015-03-03").toDate();
+		List<Bed> arrBeds1 = ((LargeCabin) lCabin).book(1, new DateTime(s1), new DateTime(e1));
+		Booking b1 = Booking.createBooking(user.id, s1, e1, lCabin.id, arrBeds1);
+		b1.save();
+		
+		Date s2 = new DateTime("2015-03-09").toDate();
+		Date e2 = new DateTime("2015-03-13").toDate();
+		List<Bed> arrBeds2 = ((LargeCabin) lCabin).book(2, new DateTime(s2), new DateTime(e2));
+		Booking b2 = Booking.createBooking(user.id, s2, e2, lCabin.id, arrBeds2);
+		b2.save();
+		
+		Date s3 = new DateTime("2015-03-23").toDate();
+		Date e3 = new DateTime("2015-03-28").toDate();
+		List<Bed> arrBeds3 = ((LargeCabin) lCabin).book(3, new DateTime(s3), new DateTime(e3));
+		Booking b3 = Booking.createBooking(user.id, s3, e3, lCabin.id, arrBeds3);
+		b3.save();
+		
+		String expectedResult = "largeCBookedDays [1,1,1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0,0,0,0,3,3,3,3,3,3]";
+		/** END adding bookings **/
+
+		Cabin cabin = Cabin.find.byId(cabinId);
+		if(cabin instanceof LargeCabin) {
+			for(Bed beds : ((LargeCabin) cabin).beds) {
+				for(Booking b : beds.bookings) {
+					int[] indices = utilities.DateHelper.getIndex(startDate, new DateTime(b.dateFrom), new DateTime(b.dateTo));
+					if(indices[0] < 0)
+						indices[0] = 0;
+					for(int i = indices[0]; i<=indices[1]; i++) {
+						largeCBookedDays[i] += 1; //TODO SHOTGUN //blankets daterange with +1 to indicate that 1 bed is taken during that period
+					}
+				}
+			}
+			//TODO test
+			actualResult = ("largeCBookedDays "+ serializer.serialize(largeCBookedDays));	
+			System.out.println(actualResult);
+		} else {
+			actualResult = (" status " + "KO ");
+			actualResult += ("message " + "date invalid");
+			System.out.println("Bad request: "+actualResult);
+		}
+		
+		assertTrue(0 == actualResult.compareTo(expectedResult));
 	}
 }
