d2abdfdfa1b6d16258d19436d79c357a3dd578b7
Olav Vatne <olavvatne@gmail.com>
2014-03-24 11:43:53 +0100

    added skeleton method required to complete transaction

--- app/controllers/PaymentController.java
+++ app/controllers/PaymentController.java
@@ -1,26 +1,110 @@
 package controllers;
 
 
+import models.Booking;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.SimpleResult;
 import static play.libs.F.Function;
 import static play.libs.F.Promise;
+import play.libs.F;
 import play.libs.WS;
 
 public class PaymentController extends Controller {
-
-	public static Promise<Result> register(Long bookingId) {
+	
+	/**
+	 * The register controller method are used for starting a payment using Netaxept.
+	 * The purpose of the register call is to send all the data needed to complete a transaction to Netaxept servers.
+	 * A call is made to Netaxept that registers a transaction and a transaction id are returned if contact is established.
+	 * This transaction id is stored in the database, and a redirect url to a Netaxept payment site is returned to the user.
+	 * 
+	 * @param bookingId - id of booking 
+	 * @return Response containing redirect url for payment.
+	 */
+	public static Promise<Result> registerPayment(Long bookingId) {
+		
+		Booking b = Booking.getBookingById(bookingId+ "");
+		if(b == null || b.status == Booking.BOOKED) {
+			return Promise.pure((Result) notFound("notfound"));
+		}
+		
+		if(b.user != SecurityController.getUser()) {
+			return Promise.pure((Result) notFound("This is not your booking"));
+		}
+		
+		
 		String url = "https://epayment-test.bbs.no/Netaxept/Register.aspx?MerchantId=319102&token=secret&orderNumber=10011&amount=200&CurrencyCode=NOK"+
-			"&redirectUrl=http://webshop/RegisterReply.asp";
+			"&redirectUrl=localhost:9000/";
+		
 		final Promise<Result> resultPromise = WS.url(url).get().map(
 				new Function<WS.Response, Result>() {
 					public Result apply(WS.Response response) {
 						return ok(response.getBody());
 					}
-
 				}
 		);
+		return resultPromise;
+	}
+	
+	/**
+	 * authenticatePayment should be called after user has filled in payment form at Netaxcept and redirected back
+	 * This method should call nets and authenticate/reserve amount on given paymentId. 
+	 * 
+	 * @param paymentId id of payment returned by nets.
+	 * @return Result with information about success/failure of payment
+	 */
+	public static Promise<Result> authenticatePayment(Long paymentId) {
+		//problem: need to know payment id- 
+		String url = "";
+
+		final Promise<Result> resultPromise = WS.url(url).get().map(
+				new Function<WS.Response, Result>() {
+					public Result apply(WS.Response response) {
+						return ok("status of payment. Went through?");
+					}
+				}
+				);
+		return resultPromise;
+	}
+	
+	
+	/**
+	 * Method will capture amount reserved with paymentId. The method calls nets and payment are 
+	 * withdrawn from customers card. 
+	 * @param paymentId
+	 * @return
+	 */
+	public static Promise<Result> capturePayment(Long paymentId) {
 		
+		String url = "";
+
+		final Promise<Result> resultPromise = WS.url(url).get().map(
+				new Function<WS.Response, Result>() {
+					public Result apply(WS.Response response) {
+						return ok("captured payment?");
+					}
+				}
+				);
+		return resultPromise;
+	}
+	
+	/**
+	 * Before payment has been captured, a user can cancel a booking. The cancelPayment should call nets and annul the reserved amount
+	 * for the booking. 
+	 * @param paymentId
+	 * @return
+	 */
+public static Promise<Result> cancelPayment(Long paymentId) {
+		
+		String url = "";
+
+		final Promise<Result> resultPromise = WS.url(url).get().map(
+				new Function<WS.Response, Result>() {
+					public Result apply(WS.Response response) {
+						return ok("anulled payment?");
+					}
+				}
+				);
 		return resultPromise;
 	}
 }
