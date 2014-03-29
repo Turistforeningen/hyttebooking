282ade12bd9bef09cdce59c1cffdf3498bf64279
Olav Vatne <olavvatne@gmail.com>
2014-03-30 00:19:16 +0100

    Moved content of public folder into appUI folder, and will work as the front end source folder. Added grunt task runner, and bower dependecy manage, and karma unit tester. These tools will be useful for unit testing, documentation, dependencies and compressing the front end into the public folder

---
+++ appUI/app/scripts/services/cabinService.js
@@ -0,0 +1,20 @@
+'use strict';
+
+app.service('cabinService', function ($http, $log) {
+    
+    
+    this.getCabins = function (page, pageSize) {
+        var url = '/api/cabins?page=' + page + "&size=" + pageSize;
+        return $http.get(url); 
+    };
+    
+    this.getCabinDetails = function (page, pageSize, id) {
+        var url = '/api/cabins/' + id+'?page=' + page + "&size=" + pageSize;
+        return $http.get(url);  
+    };
+    
+    this.postCabin = function (newCabin) {
+        var url = '/api/cabins';
+        return $http.post(url, newCabin);  
+    };
+});
