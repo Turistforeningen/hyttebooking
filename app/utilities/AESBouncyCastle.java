9517302c21fe9e1d1c8e4b0e450d12839a8d768b
jhnoor <jamawadi@gmail.com>
2014-04-29 22:55:12 +0200

    HMAC enabled, with secretKey

--- app/utilities/AESBouncyCastle.java
+++ app/utilities/AESBouncyCastle.java
@@ -1,13 +1,17 @@
 package utilities;
 
 import javax.crypto.Cipher;
+import javax.crypto.Mac;
 
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.Security;
 import java.util.Arrays;
+
 import org.apache.commons.lang3.ArrayUtils;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
@@ -17,7 +21,7 @@ import javax.xml.bind.DatatypeConverter;
  * YOU MUST INSTALL Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files
  */
 public class AESBouncyCastle {
-	
+
 	public final static int IV_BLOCK_SIZE = 16;
 
 	private byte[] keyBytes;
@@ -29,19 +33,19 @@ public class AESBouncyCastle {
 	//TODO remove
 	private byte[] testIv = DatatypeConverter.parseBase64Binary("EdZ8Ivcfug+V3lsCdB2oVw==");
 
-	
+
 	public AESBouncyCastle(byte[] keyBytes) throws Exception  {
 		if(keyBytes.length != 32) 
 			throw new Exception("Incorrect key size : "+keyBytes.length);
-		
+
 		Security.addProvider(new BouncyCastleProvider());
-		
+
 		this.keyBytes = keyBytes;
 		this.cipher = Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
 		this.key = new SecretKeySpec(keyBytes, "AES");
 		this.iv = new IvParameterSpec(new byte[IV_BLOCK_SIZE]);
 	}
-	
+
 	/**
 	 * Encrypts input and prepends iv
 	 * @return iv + cipherText byte array
@@ -52,13 +56,13 @@ public class AESBouncyCastle {
 			return null;
 		}
 		this.prevPText = input;
-		
+
 		cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(testIv));
 		byte[] cipherText = cipher.doFinal(input);
 		byte[] iv = cipher.getIV();
 		this.prevIv = iv;
 		byte[] ivAndCipherText = ArrayUtils.addAll(iv, cipherText);
-		
+
 		System.out.println();
 		System.out.println("######## encryption start ########");
 		System.out.println("IV IS: "+DatatypeConverter.printBase64Binary(iv));
@@ -67,10 +71,10 @@ public class AESBouncyCastle {
 		System.out.println("ivAndCipherText:"+ivAndCipherText.length+" : "+DatatypeConverter.printBase64Binary(ivAndCipherText));
 		System.out.println("######## encryption end   ########");
 		System.out.println();
-		
+
 		return ivAndCipherText;
 	}
-	
+
 	/**
 	 * @return the decrypt as byte array
 	 */
@@ -79,12 +83,12 @@ public class AESBouncyCastle {
 			System.err.println("Error: Data sent to be decrypted is null");
 			return null;
 		}
-	
+
 		byte[] iv = Arrays.copyOf(ivAndCipherText, IV_BLOCK_SIZE); //take first IV_BLOCK_SIZE bytes from ivAndCipherText
 		byte[] cipherText = Arrays.copyOfRange(ivAndCipherText, IV_BLOCK_SIZE, ivAndCipherText.length); //bytes from IV_BLOCK_SIZE+1 and onwards (i.e. everything after prepended iv)
 		cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv)); 
 		byte[] plainText = cipher.doFinal(cipherText); 
-		
+
 		System.out.println();
 		System.out.println("######## decryption start ########");
 		System.out.println("Iv size: "+iv.length);
@@ -95,22 +99,36 @@ public class AESBouncyCastle {
 		System.out.println("plainText: "+new String(plainText, "UTF-8"));
 		System.out.println("######## encryption end   ########");
 		System.out.println();
-		
+
 		return plainText;
 	}	
-	
+
 	/**
 	 * Hashes data and then base64 encodes it
 	 */
 	public String sha512AndBase64(byte[] data) {
-		MessageDigest md;
 		try {
+			/*
 			md = MessageDigest.getInstance("SHA-512");
 			//md.update(keyBytes); maybe should be salted with key?
 			byte[] hash = md.digest(data);
+			Mac mac = Mac.getInstance("HmacSHA512");
+			mac.init(key);
+			hmacData = mac.doFinal(data);
 			//System.out.println("hash: "+DatatypeConverter.printBase64Binary(hash));
-			return DatatypeConverter.printBase64Binary(hash);
+			return DatatypeConverter.printBase64Binary(hmacData);
+			 */
+
+			Mac sha512_HMAC = Mac.getInstance("HmacSHA512");
+			SecretKeySpec secret_key = new SecretKeySpec(keyBytes, "HmacSHA512");
+			sha512_HMAC.init(secret_key);
+
+			String hash = DatatypeConverter.printBase64Binary((sha512_HMAC.doFinal(data)));
+			return hash;
 		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		} catch (InvalidKeyException e) {
 			e.printStackTrace();
 			return null;
 		}
