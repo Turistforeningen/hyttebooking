b5fae0f025c8d2857dfda58115447780f6c2e323
jhnoor <jamawadi@gmail.com>
2014-04-30 14:46:47 +0200

    Started bookingControllerTests

--- test/models/RDate.java
+++ test/models/RDate.java
@@ -1,15 +1,12 @@
 package models;
-
 import org.joda.time.DateTime;
 
 /**
  * We want to be able to run tests and verify correctness at any time, but bookings
- * are time-sensitive, so we can't neccessarily run tests that have "2014" as date
+ * are time-sensitive, so we can't necessarily run tests that have "2014" as date
  * in the year 2015 for example.
  * So we set all date examples relative to this static object
- * 
  */
 public class RDate {
-
 	public final static DateTime fDt = new DateTime(DateTime.now().getYear()+1+"-01-01").withTimeAtStartOfDay(); /** Always set to jan 1 of the next year **/
 }
